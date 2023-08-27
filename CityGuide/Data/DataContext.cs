5bc79dffc915a36dc56b92bee7a5c845a977360b
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-08-27 14:08:05 +0300

    Created Entity

--- CityGuide/Data/DataContext.cs
+++ CityGuide/Data/DataContext.cs
@@ -8,5 +8,8 @@ namespace CityGuide.Data
     {
         public DataContext(DbContextOptions<DataContext> options):base(options) { }
         public DbSet<Value> Values{ get; set; }
+        public DbSet<City> Cities{ get; set; }
+        public DbSet<Photo> Photos{ get; set; }
+        public DbSet<User> Users{ get; set; }
     }
 }
